f0fc1e885a90b935f00c069ca657c42229c3c9ab
AndyYehSpeace <andy00307andr0id@gmail.com>
2019-05-21 10:48:15 +0800

    加入AsyncTask<String, Integer, String>類別與doinbackground方法

--- app/src/main/java/com/fragment/fragmenttest/HistoricalChartFragment.java
+++ app/src/main/java/com/fragment/fragmenttest/HistoricalChartFragment.java
@@ -2,6 +2,7 @@ package com.fragment.fragmenttest;
 
 import android.app.Activity;
 import android.net.ParseException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -18,8 +19,11 @@ import com.github.mikephil.charting.charts.LineChart;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
+
+import static android.content.ContentValues.TAG;
 
 
 public class HistoricalChartFragment extends Fragment {
@@ -27,6 +31,45 @@ public class HistoricalChartFragment extends Fragment {
 	private String value = "";
 	private LineChart chart1;
 	private LineChart chart2;
+
+
+	//regaion 變數群
+	private String[] record_id_arry;
+	private String[] sensor_id_arry;
+	private String[] sensor_category_arry;
+	private String[] sensor_value_arry;
+	private String[] read_time_arry;
+	private int ID;
+	//private Date now;
+	private android.icu.text.SimpleDateFormat sdf;
+	private int tine;
+	private Date now;
+	Date beforDate;
+	String climate;
+	String pm25;
+	String nh3;
+	String h2s;
+	String humidity;
+	String co2;
+	String ch4;
+
+	String climate_time;
+	String pm25_time;
+	String nh3_time;
+	String h2s_time;
+	String humidity_time;
+	String co2_time;
+	String ch4_time;
+
+	String climate2;
+	String pm252;
+	String nh32;
+	String h2s2;
+	String humidity2;
+	String co22;
+	String ch42;
+	//endregion
+
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -71,10 +114,40 @@ public class HistoricalChartFragment extends Fragment {
 		//txtResult.setText(value);
 	}
 
+	private class PigsAppsTask extends AsyncTask<String, Integer, String> {
 
+		//@Override
+		protected void onPreExecute() {
+//TODOAuto-generated method stub
+			Log.d(TAG, "onPreExecute");
+			//super.onPreExecute();
+		}
+
+		//@Override
+		protected String doInBackground(String... params) {
+		//TODOAuto-generated method stub
+			Log.d(TAG, "doInBackground params[0]=" + params[0]);
+			//publishProgress(10);
+			//return100L;
+			return (String) "";
+		}
+
+		//@Override
+		protected void onProgressUpdate(Integer... values) {
+//TODOAuto-generated method stub
+			Log.d(TAG, "onProgressUpdate values[0]=" + values[0]);
+			//super.onProgressUpdate(values);
+		}
+		//@Override
+		protected void onPostExecute(String result) {
+		//TODOAuto-generated method stub
+			Log.d(TAG, "onPostExecute result=" + result);
+			//super.onPostExecute(result);
+		}
+	}
 
 	//regaion 取整點的方式
-	private  String getInitialTime(String time) {
+	private String getInitialTime(String time) {
 		String hour = "00";//小时
 		String minutes = "00";//分钟
 		String outTime = "00:00";
